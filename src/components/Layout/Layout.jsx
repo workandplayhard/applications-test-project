cd5b528816b592a7bd2c2e00616d146f5bb8088d
workandplayhard <gabryalzin@gmail.com>
2023-02-05 04:09:27 -0500

    Fixed header and display empty list

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,16 +1,46 @@
-import { Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Header from "./Header";
-
 
 const CONTAINER_SX = {
   paddingTop: (theme) => theme.spacing(3),
   paddingBottom: (theme) => theme.spacing(3),
 };
 
+const HEADER_CONTAINER_STYLES_SX = {
+  paddingTop: { md: "95px" },
+  paddingLeft: { md: "80px" },
+  color: "white",
+};
+
+const HEADER_TITLE_STYLES_SX = {
+  fontSize: "48px",
+  paddingTop: "32px",
+  lineHeight: "28px",
+  fontWeight: 700,
+};
+
+const HEADER_CONTENT_STYLES_SX = {
+  maxWidth: { md: "635px" },
+  fontSize: "18px",
+  paddingTop: "32px",
+  lineHeight: "28px",
+};
+
 const Layout = ({ children }) => {
   return (
     <>
-      <Header />
+      <Header>
+        <Box sx={HEADER_CONTAINER_STYLES_SX}>
+          <Typography sx={HEADER_TITLE_STYLES_SX} variant="h1">
+            Integrations
+          </Typography>
+          <Typography sx={HEADER_CONTENT_STYLES_SX}>
+            We plan to connect you with your favorite software. If there’s an
+            app you’d love to integrate with us, let us know and we’ll notify
+            you as soon as it’s available!
+          </Typography>
+        </Box>
+      </Header>
       <Container component="main" maxWidth={"lg"} sx={CONTAINER_SX}>
         {children}
       </Container>
