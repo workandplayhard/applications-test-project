f12b895c68a0e0a2ad66ce5bdb5b46580e2411c7
workandplayhard <gabryalzin@gmail.com>
2023-02-05 02:23:29 -0500

    Close filter dropdown when click outside

--- src/components/ListFilterDialog/ListFilterDialog.jsx
+++ src/components/ListFilterDialog/ListFilterDialog.jsx
@@ -2,6 +2,7 @@ import { useCallback } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import {
   Button,
+  ClickAwayListener,
   DialogActions,
   DialogContent,
   DialogTitle,
@@ -14,7 +15,6 @@ import {
   Typography,
 } from "@mui/material";
 import theme from "./theme";
-
 
 const ListFilterDialog = ({
   anchorEl,
@@ -36,54 +36,58 @@ const ListFilterDialog = ({
 
   return (
     <ThemeProvider theme={theme}>
-      <Popper
-        open={open}
-        anchorEl={anchorEl}
-        transition
-        placement={"bottom-end"}
-      >
-        {({ TransitionProps }) => (
-          <Fade {...TransitionProps} timeout={240}>
-            <Paper data-testid="list-filter-dialog">
-              <DialogTitle>
-                <Typography component="span" variant="h6">
-                  Filters
-                </Typography>
-                <IconButton
-                  data-testid="btn-close"
-                  title="Close"
-                  onClick={onClose}
-                  disableRipple
-                  disableFocusRipple
-                >
-                  <CloseIcon />
-                </IconButton>
-              </DialogTitle>
-              <DialogContent>
-                <Stack direction="row" justifyContent="space-between">
-                  <Typography data-testid="filters-name" component="div">
-                    {title}
-                  </Typography>
-                  <Button onClick={onClear} size="small" variant="text">
-                    Clear
-                  </Button>
-                </Stack>
-                {children}
-              </DialogContent>
-              <DialogActions>
-                <Button onClick={onClear}>Clear all</Button>
-                <Button
-                  size="large"
-                  onClick={handleApplyClick}
-                  variant="contained"
-                >
-                  Apply
-                </Button>
-              </DialogActions>
-            </Paper>
-          </Fade>
-        )}
-      </Popper>
+      {open && (
+        <ClickAwayListener onClickAway={onClose}>
+          <Popper
+            open={open}
+            anchorEl={anchorEl}
+            transition
+            placement={"bottom-end"}
+          >
+            {({ TransitionProps }) => (
+              <Fade {...TransitionProps} timeout={240}>
+                <Paper data-testid="list-filter-dialog">
+                  <DialogTitle>
+                    <Typography component="span" variant="h6">
+                      Filters
+                    </Typography>
+                    <IconButton
+                      data-testid="btn-close"
+                      title="Close"
+                      onClick={onClose}
+                      disableRipple
+                      disableFocusRipple
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                  </DialogTitle>
+                  <DialogContent>
+                    <Stack direction="row" justifyContent="space-between">
+                      <Typography data-testid="filters-name" component="div">
+                        {title}
+                      </Typography>
+                      <Button onClick={onClear} size="small" variant="text">
+                        Clear
+                      </Button>
+                    </Stack>
+                    {children}
+                  </DialogContent>
+                  <DialogActions>
+                    <Button onClick={onClear}>Clear all</Button>
+                    <Button
+                      size="large"
+                      onClick={handleApplyClick}
+                      variant="contained"
+                    >
+                      Apply
+                    </Button>
+                  </DialogActions>
+                </Paper>
+              </Fade>
+            )}
+          </Popper>
+        </ClickAwayListener>
+      )}
     </ThemeProvider>
   );
 };
